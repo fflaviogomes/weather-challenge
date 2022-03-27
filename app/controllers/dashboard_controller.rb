3cfea375a6261f216a04f0e2d16c00b7b3db2442
Flavio Gomes <flaviogomes@gmail.com>
2022-03-27 07:07:45 -0300

    1-Mostrar as temperaturas também em graus Celsius

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -1,5 +1,31 @@
 class DashboardController < ApplicationController
   def index
-    @weather = Openweather2.get_weather(city: 'Sao Paulo,BR', units: 'imperial')
+    @weather = get_weather2(city: 'Sao Paulo,BR', units: 'imperial')
+    #@weather = Openweather2.get_weather(city: 'Belo Horizonte, BR', units: 'metric')
+
   end
+end
+
+#Acredito que essas funções deveriam estar em models. Não soube localizar exatamente onde colocá-la no Rails.
+
+def get_weather2 (options={})
+
+  #Chamada da função original 
+  @weather = Openweather2.get_weather(options);
+
+  #Conversão de F para C
+  def @weather.temperatureC;     temp_f_to_c(self.temperature)     end
+  def @weather.min_temperatureC; temp_f_to_c(self.min_temperature) end
+  def @weather.max_temperatureC; temp_f_to_c(self.max_temperature) end
+
+  return @weather
+
+end
+
+def temp_f_to_c(temperatureF, decimals = 2)
+
+  temperatureC = (temperatureF - 32) / 1.8;
+
+  return temperatureC.round(decimals)
+
 end
