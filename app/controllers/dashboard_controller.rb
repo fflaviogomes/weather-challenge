92e13833d3ef4b80c983199f1d3a247e78e24b4f
Flavio Gomes <flaviogomes@gmail.com>
2022-03-27 07:08:57 -0300

    1.1-Aceitar também Kelvin ou Celsius

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -13,10 +13,24 @@ def get_weather2 (options={})
   #Chamada da função original 
   @weather = Openweather2.get_weather(options);
 
-  #Conversão de F para C
-  def @weather.temperatureC;     temp_f_to_c(self.temperature)     end
-  def @weather.min_temperatureC; temp_f_to_c(self.min_temperature) end
-  def @weather.max_temperatureC; temp_f_to_c(self.max_temperature) end
+  #Conversão de F/K para C e vice-versa
+  case options[:units]
+  when 'imperial'
+    def @weather.temperatureC;     temp_f_to_c(self.temperature)     end
+    def @weather.min_temperatureC; temp_f_to_c(self.min_temperature) end
+    def @weather.max_temperatureC; temp_f_to_c(self.max_temperature) end
+
+  when 'metric'
+    def @weather.temperatureC;     self.temperature     end
+    def @weather.min_temperatureC; self.min_temperature end
+    def @weather.max_temperatureC; self.max_temperature end
+
+  else
+    def @weather.temperatureC;     temp_k_to_c(self.temperature)     end
+    def @weather.min_temperatureC; temp_k_to_c(self.min_temperature) end
+    def @weather.max_temperatureC; temp_k_to_c(self.max_temperature) end
+
+  end
 
   return @weather
 
@@ -25,6 +39,14 @@ end
 def temp_f_to_c(temperatureF, decimals = 2)
 
   temperatureC = (temperatureF - 32) / 1.8;
+
+  return temperatureC.round(decimals)
+
+end
+
+def temp_k_to_c(temperatureK, decimals = 2)
+
+  temperatureC = (temperatureK - 273);
 
   return temperatureC.round(decimals)
 
