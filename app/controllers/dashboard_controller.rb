9c45b98f6c1764a0fb60c24a8382b4901d4daf9d
Flavio Gomes <flaviogomes@gmail.com>
2022-03-27 07:22:36 -0300

    1.2-Classe tempConv

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -16,9 +16,9 @@ def get_weather2 (options={})
   #Conversão de F/K para C e vice-versa
   case options[:units]
   when 'imperial'
-    def @weather.temperatureC;     temp_f_to_c(self.temperature)     end
-    def @weather.min_temperatureC; temp_f_to_c(self.min_temperature) end
-    def @weather.max_temperatureC; temp_f_to_c(self.max_temperature) end
+    def @weather.temperatureC;     TempConv.f_to_c(self.temperature)     end
+    def @weather.min_temperatureC; TempConv.f_to_c(self.min_temperature) end
+    def @weather.max_temperatureC; TempConv.f_to_c(self.max_temperature) end
 
   when 'metric'
     def @weather.temperatureC;     self.temperature     end
@@ -26,9 +26,9 @@ def get_weather2 (options={})
     def @weather.max_temperatureC; self.max_temperature end
 
   else
-    def @weather.temperatureC;     temp_k_to_c(self.temperature)     end
-    def @weather.min_temperatureC; temp_k_to_c(self.min_temperature) end
-    def @weather.max_temperatureC; temp_k_to_c(self.max_temperature) end
+    def @weather.temperatureC;     TempConv.k_to_c(self.temperature)     end
+    def @weather.min_temperatureC; TempConv.k_to_c(self.min_temperature) end
+    def @weather.max_temperatureC; TempConv.k_to_c(self.max_temperature) end
 
   end
 
@@ -36,18 +36,23 @@ def get_weather2 (options={})
 
 end
 
-def temp_f_to_c(temperatureF, decimals = 2)
 
-  temperatureC = (temperatureF - 32) / 1.8;
+class TempConv
 
-  return temperatureC.round(decimals)
+  def self.f_to_c(temperatureF, decimals = 2)
 
-end
+    temperatureC = (temperatureF - 32) / 1.8;
 
-def temp_k_to_c(temperatureK, decimals = 2)
+    return temperatureC.round(decimals)
 
-  temperatureC = (temperatureK - 273);
+  end
 
-  return temperatureC.round(decimals)
+  def self.k_to_c(temperatureK, decimals = 2)
+
+    temperatureC = (temperatureK - 273);
+
+    return temperatureC.round(decimals)
+
+  end
 
 end
